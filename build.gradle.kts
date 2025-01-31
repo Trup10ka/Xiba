3059cf097ee7bdb86030249ad2a1da57628c711f
Trup10ka <lukys3036@gmail.com>
2025-01-31 21:40:19 +0100

    Add strict java toolchain version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
+}
 
 tasks.test {
     useJUnitPlatform()

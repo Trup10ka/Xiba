9e1eff5d1e46e48d744cc713ecd328e59cc19656
Trup10ka <lukys3036@gmail.com>
2025-02-02 23:37:26 +0100

    Add command abstract class

---
+++ src/main/java/com/trup10ka/xiba/commands/Command.java
@@ -0,0 +1,43 @@
+package com.trup10ka.xiba.commands;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * This class was reused from the previous project Kappa
+ * @see <a href="https://github.com/Trup10ka/Kappa/blob/main/src/main/java/com/trup10ka/kappa/cli/commands/Command.java">Kappa - Command</a>
+ */
+public abstract class Command
+{
+    @NotNull
+    private final CommandIdentifier identifier;
+
+    private String shortDescription;
+
+    public Command(@NotNull CommandIdentifier identifier)
+    {
+        this.identifier = identifier;
+    }
+
+    @NotNull
+    public abstract String execute(@Nullable String args);
+
+    @NotNull
+    public CommandIdentifier getIdentifier()
+    {
+        return identifier;
+    }
+
+    protected void setShortDescription(String shortDescription)
+    {
+        this.shortDescription = shortDescription;
+    }
+
+    @NotNull
+    public String getShortDescription()
+    {
+        return shortDescription;
+    }
+
+    public abstract String getHelp();
+}
